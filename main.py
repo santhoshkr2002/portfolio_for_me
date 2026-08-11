4c1e49a36e8518d4e7b7268aa9dd8ec2a40d421c
santhoshkr2002 <santhoshkr662002@gmail.com>
2026-08-11 22:04:07 +0530

    docker and main.py changed

--- main.py
+++ main.py
@@ -94,5 +94,5 @@ def clear(session_id):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
